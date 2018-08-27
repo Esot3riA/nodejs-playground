70cfc32818a0efbf4ffccd658bdf46d413970a93
Esot3riA <esot3ria@gmail.com>
2018-08-27 23:24:39 +0000

    First upload webapp

--- src/1-nodebeginner/1-firstwebapp/server.js
+++ src/1-nodebeginner/1-firstwebapp/server.js
@@ -9,12 +9,7 @@ function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		
-		var content = route(handle, pathname);
-		
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(content);
-		response.end();
+		route(handle, pathname, response, request);
 	}
 	
 	http.createServer(onRequest).listen(3000);

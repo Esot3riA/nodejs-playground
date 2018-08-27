27b2cfdf0d5025db9a9800e960900f8099813c71
Esot3riA <esot3ria@gmail.com>
2018-08-27 14:56:09 +0000

    First webapp

--- src/1-nodebeginner/1-firstwebapp/server.js
+++ src/1-nodebeginner/1-firstwebapp/server.js
@@ -5,15 +5,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		
-		route(pathname);
+		var content = route(handle, pathname);
 		
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("hello, world");
+		response.write(content);
 		response.end();
 	}
 	

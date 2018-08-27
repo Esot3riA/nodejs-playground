3d2b715ed3953b4f13fa0e8d576552da8130bf58
Esot3riA <esot3ria@gmail.com>
2018-08-27 14:26:35 +0000

    First webapp

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 There is node.js playground of Esot3riA!
 
 Useful tutorial links
-- https://www.nodebeginner.org/index-kr.html
-- https://velopert.com/node-js-tutorials
-- https://www.w3schools.com/nodejs/default.asp
+1. https://www.nodebeginner.org/index-kr.html
+2. https://velopert.com/node-js-tutorials
+3. https://www.w3schools.com/nodejs/default.asp
--- src/1-nodebeginner/1-firstwebapp/server.js
+++ src/1-nodebeginner/1-firstwebapp/server.js
@@ -3,11 +3,22 @@
 // port is fixed to 3000!
 
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("hello, world\n");
-	response.write("it is working!");
-	response.end();
-}).listen(3000, function() {});
-// Why need empty function?
+function start(route) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		
+		route(pathname);
+		
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("hello, world");
+		response.end();
+	}
+	
+	http.createServer(onRequest).listen(3000);
+	console.log("Server has started.");
+}
+
+exports.start = start;

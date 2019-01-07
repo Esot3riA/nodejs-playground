a0db2d8cddd010e1b921013d0ae3ee2d50faf346
Esot3riA <esot3ria@gmail.com>
2019-01-07 14:20:21 +0000

    Apply react-actions and Use Ducks structure

--- src/2-velopert/8-redux/redux-counter/src/index.js
+++ src/2-velopert/8-redux/redux-counter/src/index.js
@@ -5,7 +5,7 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux';
-import reducers from './reducers';
+import reducers from './modules';
 import { Provider } from 'react-redux';
 
 const store = createStore(reducers, 
